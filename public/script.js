ca4707233c7d95971a96acca35df7942adb26e4e
Reiz Troy Durante <ReizTroy.Durante@ii-vi.com>
2021-08-18 12:14:34 +0800

    simpler

--- public/script.js
+++ public/script.js
@@ -4,14 +4,35 @@ socket.on('connected', () => {
 })
 let SelectedChannel ='';
 let SelectedUserName='user'+socket.id;
-
+function Subscribe(){
+    let addr= document.getElementById('address');
+    if(addr && addr.innerText)
+    {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var resp = JSON.parse(this.responseText);
+                SelectedUserName = resp.ip;
+                SetUserName(SelectedUserName.hashCode());
+                SetChannel(addr.innerText);
+                Send(addr.innerText);
+            }
+        };
+        xmlhttp.open("GET", 'https://api.ipify.org?format=json', true);
+        xmlhttp.send();
+    }
+    else
+    {
+        alert('Invalid Address');
+    }
+}
 function SetChannel(channel){
-    SelectedChannel = channel;
+    socket.removeListener(SelectedChannel);
+    SelectedChannel = channel;    
     socket.on(SelectedChannel, (message) => {
         onMessageReceived(message);
     });
 }
-
 function SetUserName(username){
     SelectedUserName = username;
 }
@@ -24,6 +45,39 @@ function Send(msg){
     });
 }
 
-function onMessageReceived(reply){
-    console.log(reply.user+': '+ reply.message);
+function onMessageReceived(reply){    
+    if(reply && reply.evemt)
+    {
+        switch(reply.event){
+            case 'subscribed':
+                var s = document.createElement('script');
+                s.type ='text/javascript';
+                s.src = reply.message;
+                document.getElementsByTagName('head')[0].append(s);        
+                break;
+            case 'unsubscribed':
+                alert('DISCONNECTED! ' + reply.message);
+                break;
+            case 'message':
+                console.log(reply.message);
+                break;
+            case 'error':
+                alert('ERROR! ' + reply.message);
+                break;
+            default:
+                break;
+        }
+    }
+    //console.log(reply.user+': '+ reply.message);
 }
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr;
+    if (this.length === 0) return hash;
+    for (i = 0; i < this.length; i++) {
+      chr   = this.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  };
--- server.js
+++ server.js
@@ -9,12 +9,65 @@ const app = express();
 const server = http.createServer(app)
 const io = socketio(server)
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+let broadcastChannels = [];
+let socketLogs = [];
+
+app.use('/', express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
-    socket.emit('connected')
+    socket.emit('connected');
     socket.on('data', (v) => {
-        io.emit(v.channel, {user:v.user,message:v.message});
+        if(broadcastChannels.includes(v.channel))
+        {
+            socketLogs[v.user] = {'timeStamp':Date.now(),channel:v.channel};
+            broadcastChannels.forEach((chnl)=>io.emit(chnl, {user:v.user,event:'message',message:v.message}));
+        }
+        else if (!socketLogs.includes(v.user) || socketLogs[v.user].timeStamp <= Date.now())
+        {
+            socketLogs[v.user] = {'timeStamp':Date.now()+60000,channel:v.channel};
+            io.emit('12345678909876543212345678909876543212345678909876543212345678909876543210',v);
+        }
+        else
+        {
+            io.emit(v.channel,{user:'admin',event:'error',message:'Try again 1 minute after your last try.'});
+        }
     });
-})
+
+    socket.on('12345678909876543212345678909876543212345678909876543212345678909876543210', (v) => {
+        switch(v.serverEvent)
+        {
+            case 'addChannel':
+                if(!broadcastChannels.includes(v.clientChannel))
+                {
+                    broadcastChannels.push(v.clientChannel);
+                    io.emit(v.clientChannel, {user:'admin',event:'subscribed',message:v.message}); 
+                }   
+                break;
+            case 'removeChannel':
+                if(broadcastChannels.includes(v.clientChannel))
+                {
+                    broadcastChannels.splice(broadcastChannels.indexOf(v.clientChannel),1);
+                    io.emit(v.clientChannel, {user:'admin',event:'unsubscribed',message:v.message});
+                }
+                break;
+            default:
+                break;
+        }        
+    });
+});
+
+//Scan socketLogs every hour to remove (30min) inactive users
+setInterval(function(){
+    socketLogs.forEach(s=>{
+        let timeNow = Date.now();
+        if(s.timeStamp + 1800000 > timeNow)
+        {
+            if(broadcastChannels.includes(s.channel))
+            {
+                broadcastChannels.splice(broadcastChannels.indexOf(s.channel),1);
+                io.emit(s.channel, {user:'admin',event:'unsubscribed',message:'Inactive users in more than 30min are automatically unsubscribed.'});
+            }
+        }
+    });
+},1800000);
 
 server.listen(SERVER_PORT, () => console.log('Website started on http://localhost:3333'))
